bba1fb6ced196f71fcafe22b6e7311cac637bc7e
philmurtagh101 <philmurtagh101@gmail.com>
2021-06-06 13:51:44 +0000

    Pagination and tables sorted

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var data;
-const baseURL = "https://ci-swapi.herokuapp.com/api/";
+//const baseURL = "https://ci-swapi.herokuapp.com/api/";
 
-function getData(type, cb) {
+function getData(url, cb) {
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", baseURL + type + "/");
+    xhr.open("GET", url);
     xhr.send();
 
     xhr.onreadystatechange = function () {
@@ -13,14 +13,50 @@ function getData(type, cb) {
     };
 }
 
-function writeToDocument(type) {
+function getTableHeaders(obj){
+    var tableHeaders = [];
+
+    Object.keys(obj).forEach(function(key){
+        tableHeaders.push(`<th>${key}</th>`);
+    })
+    return `<tr>${tableHeaders}</tr>`;
+}
+
+function generatePaginationButtons(next, prev){
+    if (next && prev) {
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>
+                <button onclick="writeToDocument('${next}')">Next</button>`;
+    } else if(next&& !prev){
+        return `<button onclick="writeToDocument('${next}')">Next</button>`;
+    } else if(!next && prev){
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>`;
+    }
+}
+
+function writeToDocument(url) {
+    var tableRows = [];
     var el=document.getElementById("data");
     el.innerHTML=""; //this blanks the element if id data
-    getData(type, function (data) {
-        data = data.results
+    getData(url, function (data) {
+        var pagination;
+        if(data.next || data.previous){
+            pagination = generatePaginationButtons(data.next, data.previous);
+        }
+
+        data = data.results;
+        var tableHeaders = getTableHeaders(data[0])
+
         data.forEach(function(item){
-            el.innerHTML += "<p>" + item.name + "</p>";
+            var dataRow=[];
+            Object.keys(item).forEach(function(key){
+                var rowData = item[key].toString();
+                var truncatedData = rowData.substring(0,15);
+                dataRow.push(`<td>${truncatedData}</td>`);
+            });
+            tableRows.push(`<tr>${dataRow}</tr>`);
         });
+           // el.innerHTML += "<p>" + item.name + "</p>";
         
+        el.innerHTML=`<table>${tableHeaders}${tableRows}</table>${pagination}`;
     });
 }
